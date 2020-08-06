86d1c13140930e28c7f6d0bc6318cb887ad63fe8
Bhanu3699653 <64768759+Bhanu3699653@users.noreply.github.com>
2020-08-06 10:10:33 -0700

    Add files via upload

---
+++ Java Codes/CustomerDao.java
@@ -0,0 +1,34 @@
+package com.nani;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class CustomerDao {
+public int customerdb(Customer customer) throws ClassNotFoundException {
+		
+		String str3 = "insert into transactiondata values(?,?,?,?);";
+		int result = 0;
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		try(Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankapplication", "student", "student");
+				PreparedStatement ps = myConn.prepareStatement(str3)) {
+			  ps.setInt(1,customer.getAcno());
+			  ps.setString(2,customer.getDateoftx());
+			  ps.setInt(3,customer.getCredit());
+			  ps.setInt(4,customer.getWithdraw());
+			  
+			 
+			System.out.println(ps);
+			
+			result = ps.executeUpdate();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		System.out.println(result);
+
+		return result;
+	}
+}
